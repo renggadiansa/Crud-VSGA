a7b402e58505857fb86f16dbeed22a39123e88f8
renggadiansa <renggadiansa@gmail.com>
2023-08-13 20:09:22 +0700

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Crud VSGA 2023"
+include(":app")
